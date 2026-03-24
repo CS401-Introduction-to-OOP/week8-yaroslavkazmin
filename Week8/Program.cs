5fa3363c5d85c40da5a29333c135736f2f7579d5
Yaroslav Kazmin <yaroslav.kazmin.25@kse.org.ua>
2026-03-24 19:30:02 +0200

    закінчив третій етап з Program.cs, тільки не знаімайте бал за десятихвилинне запізнення

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -2,5 +2,19 @@
 class Program
 {
     static void Main()
-    {}
+    {
+        var Letter1 = new Letter("1", 0.01);
+        var Letter2 = new Letter("2", 0.02);
+        var Parcel1 = new Parcel("3", 10);
+        var Parcel2 = new Parcel("4", 20);
+        
+        Console.WriteLine(Letter1);
+        Console.WriteLine(Parcel1);
+        CargoContainer<DeliveryItem> myCargo = new CargoContainer<DeliveryItem>();
+        myCargo.AddItem(Letter1);
+        myCargo.AddItem(Letter2);
+        myCargo.AddItem(Parcel1);
+        myCargo.AddItem(Parcel2);
+        Console.WriteLine(myCargo.GetTotalCost());
+    }
 }
